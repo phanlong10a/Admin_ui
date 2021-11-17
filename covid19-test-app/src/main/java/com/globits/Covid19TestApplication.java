ae3717b358cd459d2bd4dc03dc81c527aacfe483
Phan Long <>
2021-11-17 19:28:56 +0700

    backend covid

---
+++ covid19-test-app/src/main/java/com/globits/Covid19TestApplication.java
@@ -0,0 +1,17 @@
+package com.globits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan
+@EnableScheduling
+public class Covid19TestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19TestApplication.class, args);
+	}
+
+}
